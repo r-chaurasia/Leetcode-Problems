456cd52771c277a99441c3c53eead2fa98092fce
Riya Chaurasia <riyachaurasia0107@gmail.com>
2025-09-18 16:51:45 +0530

    PowerOfTwo.cpp

--- PowerOfTwo.cpp
+++ PowerOfTwo.cpp
@@ -3,6 +3,8 @@ public:
     bool isPowerOfTwo(int n) {
         if (n == 1)
             return true;
+        if (n == 0)
+            return false;
         if (n % 2 != 0)
             return false;
 
