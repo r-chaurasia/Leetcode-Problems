30a5bb049113c6dfb83eab86e2fdca8a41471790
Riya Chaurasia <riyachaurasia0107@gmail.com>
2025-09-19 17:40:08 +0000

    container config done

---
+++ BinaryTreePreorderTraversal.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    void pre(TreeNode *root , vector<int>&ans) {
+        if (!root)
+            return;
+        
+        ans.push_back(root->val);
+        pre(root->left , ans);
+        pre(root->right , ans);
+    }
+    vector<int> preorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        pre(root , ans);
+
+        return ans;
+    }
+};
