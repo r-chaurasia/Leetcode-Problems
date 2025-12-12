57f8fcb557401458cc95e15caf85f87adeba74c5
Riya Chaurasia <riyachaurasia0107@gmail.com>
2025-12-12 15:33:56 +0000

    container config

---
+++ 3Sum.cpp
@@ -0,0 +1,69 @@
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        int n = nums.size();
+        set<vector<int>> st;
+
+        // brute force - tc = O(n3 * log(no of triplets))
+        // for (int i = 0 ; i < n ; i++) {
+        //     for (int j = i+1 ; j < n ; j++) {
+        //         for (int k = j+1 ; k < n ; k++) {
+        //             if (nums[i] + nums[j] + nums[k] == 0) {
+        //                 vector<int> temp = {nums[i] , nums[j] , nums[k]};
+        //                 sort(temp.begin() , temp.end());
+        //                 st.insert(temp);
+        //             }
+        //         }
+        //     }
+        // }
+
+        // better - tc = O(n2 * log(m)) ; m = size of hashset
+        // for (int i = 0 ; i < n ; i++) {
+        //     set<int> hashset;
+        //     for (int j = i+1 ; j < n ; j++) {
+        //         int third = -(nums[i] + nums[j]);
+        //         if (hashset.find(third) != hashset.end()) {
+        //             vector<int> temp = {nums[i] , nums[j] , third};
+        //             sort(temp.begin() , temp.end());
+        //             st.insert(temp);
+        //         }
+        //         hashset.insert(nums[j]);
+        //     }
+        // }
+        // vector<vector<int>> ans(st.begin() , st.end());
+        // return ans;
+
+        // optimal - tc = O(nlogn + n*n) and sc = O(no of triplets)
+        vector<vector<int>> ans;
+        sort(nums.begin() , nums.end());
+
+        for (int i = 0 ; i < n ; i++) {
+            if (i>0 && nums[i] == nums[i-1])
+                continue;
+            int j = i+1;
+            int k = n-1;
+
+            while (j < k) {
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum > 0) {
+                    k--;
+                }
+                else if (sum < 0) {
+                    j++;
+                }
+                else {
+                    vector<int> temp = {nums[i] , nums[j] , nums[k]};
+                    ans.push_back(temp);
+                    j++;
+                    k--;
+                    while (j < k && nums[j] == nums[j-1])
+                        j++;
+                    while (j < k && nums[k] == nums[k+1])
+                        k--;
+                }
+            }
+        }
+
+        return ans;
+    }
+};
