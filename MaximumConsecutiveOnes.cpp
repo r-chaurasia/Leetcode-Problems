30129c524fa9826d71a044cd79d76b3344ab560d
Riya Chaurasia <riyachaurasia0107@gmail.com>
2026-02-26 14:07:44 +0000

    container config

---
+++ MaximumConsecutiveOnes.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findMaxConsecutiveOnes(vector<int>& nums) {
+        int n = nums.size();
+        int count = 0;
+        int m = 0;
+
+        for(int i=0 ; i<n ; i++) {
+            if(nums[i] == 1)
+                count++;
+            else
+                count = 0;
+            m = max(count, m);
+        }
+
+        return m;
+    }
+};
